2b33a548f3b31c4d3a6457e396b93266e09e7f62
Cupid Valentine <westerngod@126.com>
2024-08-04 22:51:33 +0800

    feat: completed login

--- apps/service/src/user/user.controller.ts
+++ apps/service/src/user/user.controller.ts
@@ -1,10 +1,33 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Inject, Post } from '@nestjs/common';
 import { UserService } from './user.service';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
+import { JwtService } from '@nestjs/jwt';
 
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @Inject(JwtService)
+  private jwtService: JwtService;
+
+  @Post('login')
+  async userLogin(@Body() loginUser: LoginUserDto) {
+    const user = await this.userService.login(loginUser);
+
+    return {
+      user,
+      token: this.jwtService.sign(
+        {
+          userId: user.id,
+          username: user.username,
+        },
+        {
+          expiresIn: '7d',
+        },
+      ),
+    };
+  }
 
   @Post('register')
   async register(@Body() registerUser: RegisterUserDto) {
--- apps/service/src/user/user.service.ts
+++ apps/service/src/user/user.service.ts
@@ -9,19 +9,20 @@ import { Prisma } from '@prisma/client';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { RedisService } from 'src/redis/redis.service';
 import { RegisterUserDto } from './dto/register-user.dto';
+import { LoginUserDto } from './dto/login-user.dto';
 
 @Injectable()
 export class UserService {
   @Inject(PrismaService)
-  private prismaServices: PrismaService;
+  private prismaService: PrismaService;
 
   @Inject(RedisService)
-  private redisServices: RedisService;
+  private redisService: RedisService;
 
   private logger = new Logger();
 
   async create(user: RegisterUserDto) {
-    const captcha = await this.redisServices.get(`captcha_${user.email}`);
+    const captcha = await this.redisService.get(`captcha_${user.email}`);
 
     if (!captcha) {
       throw new HttpException('验证码已失效', HttpStatus.BAD_REQUEST);
@@ -31,7 +32,7 @@ export class UserService {
       throw new HttpException('验证码不正确', HttpStatus.BAD_REQUEST);
     }
 
-    const foundUser = await this.prismaServices.user.findUnique({
+    const foundUser = await this.prismaService.user.findUnique({
       where: {
         username: user.username,
       },
@@ -41,7 +42,7 @@ export class UserService {
       throw new HttpException('用户已存在', HttpStatus.BAD_REQUEST);
     }
     try {
-      return await this.prismaServices.user.create({
+      return await this.prismaService.user.create({
         data: {
           username: user.username,
           password: user.password,
@@ -61,5 +62,24 @@ export class UserService {
       this.logger.error(error, UserService);
       return null;
     }
+  }
+
+  async login(loginUserDto: LoginUserDto) {
+    const foundUser = await this.prismaService.user.findUnique({
+      where: {
+        username: loginUserDto.username,
+      },
+    });
+
+    if (!foundUser) {
+      throw new HttpException('用户不存在', HttpStatus.BAD_REQUEST);
+    }
+
+    if (foundUser.password !== loginUserDto.password) {
+      throw new HttpException('密码错误', HttpStatus.BAD_REQUEST);
+    }
+
+    delete foundUser.password;
+    return foundUser;
   }
 }
